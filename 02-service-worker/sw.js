f5d5acd6bcfa4d926494e5ce1d0635876a72e942
RamirezJosue <merling.ramirez@upeu.edu.pe>
2018-10-25 20:46:08 -0500

    fundamentos

---
+++ 02-service-worker/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('fetch', event => {
+    const resp = fetch(event.request)
+        .then(resp => resp.ok ? resp : fetch('img/main.jpg')
+            // if (resp.ok) return resp;
+            // else return fetch('img/main.jpg')  
+        );
+    event.respondWith(resp);
+});
